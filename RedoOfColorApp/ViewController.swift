4402ba38f12c87a079a528037f1de1c67ee1cf46
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-10 07:47:07 -0600

    Redid the whole color app

--- RedoOfColorApp/ViewController.swift
+++ RedoOfColorApp/ViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var Button: UIButton!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func FirstMethod(_ sender: UIButton)
+    {
+        if (Button.backgroundColor == .red)
+        {
+            Button.backgroundColor = .black
+        }
+        else
+        {
+            Button.backgroundColor = .black
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor : UIColor
+        let redAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
