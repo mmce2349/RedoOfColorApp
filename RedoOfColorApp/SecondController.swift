968d0d9f55f21bd0555acdca5cb10a7bdc69c816
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-12 09:10:09 -0600

    Added a button to go to the second screen

---
+++ RedoOfColorApp/SecondController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondController.swift
+//  RedoOfColorApp
+//
+//  Created by Mcentire, Mitchell on 10/12/17.
+//  Copyright © 2017 Mcentire, Mitchell. All rights reserved.
+//
+
+import UIKit
+class SecondController : UIViewController
+{
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
