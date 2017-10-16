2da16cb71caf29d0b901039059e65e61df6348c5
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-16 09:17:37 -0600

    Added a new view controller.

---
+++ RedoOfColorApp/ThirdController.swift
@@ -0,0 +1,13 @@
+//
+//  ThirdController.swift
+//  RedoOfColorApp
+//
+//  Created by Mcentire, Mitchell on 10/16/17.
+//  Copyright © 2017 Mcentire, Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ThirdController: UIViewController {
+
+}
